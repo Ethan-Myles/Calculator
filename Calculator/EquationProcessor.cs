c61f3f813e5df7c2d6d80f1b4d22c1251564fd98
Ethan <78313955+Ethan-Myles@users.noreply.github.com>
2024-08-29 14:16:06 +0100

    Added sample and population variance and standard deviation

--- Calculator/EquationProcessor.cs
+++ Calculator/EquationProcessor.cs
@@ -6,33 +6,140 @@ namespace Calculator
     {
         public static void EquationProcessor(string equation)
         {
+            var basicFormat = Regex.IsMatch(equation, "^[a-z]+ \\[.*\\]$");
+
+            var variationFormat = Regex.IsMatch(equation, "^[a-z]+ [a-z]+ \\[.*\\]$");
+
+            bool CheckFormats(string typeOfFormat)
+            {
+                switch (typeOfFormat)
+                {
+                    case "basic":
+
+                        if (basicFormat == false)
+                        {
+                            Console.WriteLine("\nInput in the incorrect format, check \"options\" for the formats of operations\n");
+                            return false;
+                        }
+                        else
+                        {
+                            return true;
+                        }
+
+                    case "variation":
+
+                        if (variationFormat == false)
+                        {
+                            Console.WriteLine("\nInput in the incorrect format, check \"options\" for the formats of operations\n");
+                            return false;
+                        }
+                        else
+                        {
+                            return true;
+                        }
+
+                    default:
+                        Console.WriteLine("\nAn error occured when matching the format of this input\n");
+                        return false;
+                }
+            }
 
             string[] words = equation.Split(' ');
 
             switch (words[0])
             {
                 case "mea":
-                    Console.WriteLine("\nThe mean is " + ComputeMea(words[1]) + "\n");
-                    break;
+
+                    if (CheckFormats("basic"))
+                    {
+                        Console.WriteLine("\nThe mean is " + ComputeMea(words[1]) + "\n");
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
 
                 case "med":
 
-                    Console.WriteLine("\nThe median is " + ComputeMed(words[1]) + "\n");
-                    break;
+                    if (CheckFormats("basic"))
+                    {
+                        Console.WriteLine("\nThe median is " + ComputeMed(words[1]) + "\n");
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
 
                 case "mode":
 
-                    Console.WriteLine(string.Join(", ", ComputeMode(words[1])) + "\n");
-                    break;
+                    if (CheckFormats("basic"))
+                    {
+                        Console.WriteLine(string.Join(", ", ComputeMode(words[1])) + "\n");
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
 
                 case "ran":
 
-                    Console.WriteLine("\nThe range is " + ComputeRan(words[1]) + "\n");
-                    break;
+                    if (CheckFormats("basic"))
+                    {
+                        Console.WriteLine("\nThe range is " + ComputeRan(words[1]) + "\n");
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                case "sd":
+
+                    if (CheckFormats("variation"))
+                    {
+                        Console.WriteLine("\nThe standard deviation is " + ComputeSdAndVar(words[1], words[2], true) + "\n");
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                case "var":
+
+                    if (CheckFormats("variation"))
+                    {
+                        Console.WriteLine("\nThe variance is " + ComputeSdAndVar(words[1], words[2], false) + "\n");
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
 
                 default:
                     Console.WriteLine("\nUnknown operation, check \"options\" for a list of possible operations\n");
                     break;
+            }
+
+            static double ComputeSquareRoot(double num)
+            {
+                double guess = num / 2;
+                double previousGuess;
+
+                do
+                {
+                    previousGuess = guess;
+                    guess = (guess + num / guess) / 2;
+
+                }
+                while ((guess > previousGuess ? guess - previousGuess : previousGuess - guess) > 0.0001);
+
+                return guess;
+
             }
 
             // Might be able to do this a quicker way
@@ -51,7 +158,7 @@ namespace Calculator
                 ?: - Makes the groups non-capturing, so intermiedate matches aren't uncessarily stored 
                 */
 
-                var format = Regex.IsMatch(doubleString, "^\\[(?:-?\\d+(?:\\.\\d+)?)(?:,-?\\d+(?:\\.\\d+)?)*\\]$");
+                var inputFormat = Regex.IsMatch(doubleString, "^\\[(?:-?\\d+(?:\\.\\d+)?)(?:,-?\\d+(?:\\.\\d+)?)*\\]$");
 
                 if (doubleString == "[]")
                 {
@@ -59,7 +166,7 @@ namespace Calculator
                     return Array.Empty<double>();
                 }
 
-                else if(format == false)
+                else if (inputFormat == false)
                 {
                     Console.WriteLine("\nInput in the incorrect format, check \"options\" for the formats of operations");
                     return Array.Empty<double>();
@@ -117,7 +224,7 @@ namespace Calculator
                 //odd case
                 else
                 {
-                    median = doubleArray[doubleArray.Length / 2];    
+                    median = doubleArray[doubleArray.Length / 2];
                 }
 
                 return median;
@@ -201,6 +308,57 @@ namespace Calculator
                 return range;
             }
 
+            static double ComputeSdAndVar(string sampleOrPopulation, string doublestring, bool sd)
+            {
+
+                if (sampleOrPopulation != "s" && sampleOrPopulation != "p")
+                {
+                    Console.WriteLine("\nInput in the incorrect format, check \"options\" for the formats of operations");
+                }
+
+                double[] doubleArray = StringToDoubleArray(doublestring);
+
+                if (doubleArray.Length == 0)
+                {
+                    return double.NaN;
+                }
+
+                double mean = ComputeMea(doublestring);
+
+                double numerator = 0;
+
+                double standardDeviation = 0;
+
+                foreach (double value in doubleArray)
+                {
+                    numerator += (value - mean) * (value - mean);
+                }
+
+                double variance = double.NaN;
+
+                if (sampleOrPopulation == "s")
+                {
+                    double denom = doubleArray.Length - 1;
+
+                    variance = (numerator / (doubleArray.Length - 1));
+                }
+                else if (sampleOrPopulation == "p")
+                {
+                    variance = (numerator / doubleArray.Length);
+                }
+
+                if (sd)
+                {
+                    standardDeviation = ComputeSquareRoot(variance);
+                    return standardDeviation;
+                }
+                else
+                {
+                    return variance;
+
+                }
+            }
         }
+
     }
 }
